34063bc025e6e5cbe3003b352a9ae9882338e813
Ryan Nóbrega <ryanbrandao18@gmail.com>
2021-12-07 16:45:24 -0300

    adicionando comentários na classe principal

--- src/relacionamento/Agenda.java
+++ src/relacionamento/Agenda.java
@@ -1,5 +1,36 @@
 package relacionamento;
 
 public class Agenda {
-    
+
+    private String nomeAgenda;
+    private Contato[] contatos;
+
+    public Agenda(String nomeAgenda) {
+    }
+
+    public String getNomeAgenda() {
+        return nomeAgenda;
+    }
+
+    public void setNomeAgenda(String nomeAgenda) {
+        this.nomeAgenda = nomeAgenda;
+    }
+
+
+    public Contato[] getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(Contato[] contatos) {
+        this.contatos = contatos;
+    }
+    public String obterInfo(){
+        String info = "nome = " + nomeAgenda + "\n";
+        if (contatos != null){
+            for (Contato c: contatos){
+                info += c.obterInfo() + "\n";
+            }
+        }
+        return info;
+    }
 }
--- src/relacionamento/ProgramaPrincipal.java
+++ src/relacionamento/ProgramaPrincipal.java
@@ -1,2 +1,35 @@
-package relacionamento;public class ProgramaPrincipal {
+package relacionamento;
+import java.util.Scanner;
+public class ProgramaPrincipal {
+    public static  void main(String[] ags){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o nome da sua agenda: ");
+        String nomeAgenda = scan.next();
+        Agenda agenda1 = new Agenda(nomeAgenda);
+
+        // criando um array de objetos
+        Contato[] contatos = new Contato[3];
+        // Gerando os 3 contatos atraves do for e setando os valores
+        for(int i=0; i<3; i++){
+            System.out.println("Digite as informações do contato: "+ (i+1));
+            Contato c = new Contato();
+            System.out.println("Entre com o nome: ");
+            String nome = scan.nextLine();
+            c.setNome(nome);
+            System.out.println("Entre com o email: ");
+            String email = scan.nextLine();
+            c.setEmail(email);
+            System.out.println("Entre com o telefone: ");
+            String telefone = scan.nextLine();
+            c.setTelefone(telefone);
+            contatos[i] = c;
+        }
+        agenda1.setContatos(contatos);
+        if (agenda1 != null){
+            System.out.println((agenda1.obterInfo()));
+        }
+
+
+
+    }
 }
